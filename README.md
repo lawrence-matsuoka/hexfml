c2b48758f1810bcd919eca25a6f51d479d7616df
asahi-nixos <Lawrence.Matsuoka@proton.me>
2025-03-31 16:46:37 -0300

    colours, textures, and project directory setup

--- README.md
+++ README.md
@@ -2,6 +2,35 @@
 
 Hex + SFML not hex + the acronym fml that has a much different meaning, but definitely reflects the development experience so far.
 
+## Learning outcomes
+- C++
+- Game development
+- Networking (sockets)
+
+## Project structure (WIP)
+/hexfml/
+│── /src/
+│   ├── main.cpp
+│   ├── Game.cpp
+│   ├── Game.hpp
+│   ├── Board.cpp
+│   ├── Board.hpp
+│   ├── InputHandler.cpp
+│   ├── InputHandler.hpp
+│── /include/
+│── /assets/
+│   ├── textures/
+│   ├── fonts/
+│   ├── sounds/
+│── /shaders/          # TBD
+│── /config/           # TBD
+│   ├── settings.ini
+│── /bin/
+│── /obj/
+│── Makefile
+│── README.md
+
+
 multiplayer online game of Hex. Most likely written in C++ and as barebones as possible (work on anything). Sockets? for game?
 
 sudo apt install libsfml-dev
@@ -11,3 +40,6 @@ make
 make clean
 
 ./bin/hex++
+
+## Credits
+https://opengameart.org/node/21051 for wood texture
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
+
+int rows = 11;
+int columns = 11;
 
 int main() {
   sf::RenderWindow window({1600, 900}, "hex++", sf::Style::Default,
@@ -12,6 +16,13 @@ int main() {
   hexagon.setFillColor(sf::Color::White);
   hexagon.setOrigin(radius, radius);
 
+  // Load wood texture
+  sf::Texture woodTexture;
+  if (!woodTexture.loadFromFile("assets/textures/light-wood.jpg")) {
+    return -1; // Error handling
+  }
+  hexagon.setTexture(&woodTexture);
+
   // Set an aspect ratio for dynamic window sizes
   sf::View view = window.getDefaultView(); // aspect ratio
 
@@ -20,29 +31,39 @@ int main() {
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
-      // 
+      //
       if (event.type == sf::Event::Resized) {
         float newWidth = event.size.width;
         float newHeight = event.size.height;
 
         float aspectRatio =
             static_cast<float>(newWidth) / static_cast<float>(newHeight);
-        view.setSize(800 * aspectRatio,
-                     800); 
-        view.setCenter(view.getSize().x / 2, view.getSize().y / 2);
+        view.setSize(800 * aspectRatio, 800);
+        //        view.setCenter(view.getSize().x / 2, view.getSize().y / 2);
+
+        // Centre the grid of hexagons in the middle of the window
+        float gridCentreX = (rows - 1) * 1.5f * radius;
+        float gridCentreY = (columns - 1) * radius;
+        //        float gridCentreY = (columns - 1) * radius / 2.0f;
+
+        view.setCenter(gridCentreX, gridCentreY);
+
+        view.zoom(0.55f);
         window.setView(view);
       }
     }
 
     window.clear(sf::Color::White);
 
-    int rows = 11;
-    int columns = 11;
-    int gridOffset = 36; // 35
-    int shiftGrid = gridOffset;
+    // Set background colour
+    //    window.clear(sf::Color(100, 150, 200)); // light blue
+    window.clear(sf::Color(85, 115, 85)); // Dark sage green
+    //    window.clear(sf::Color(120, 120, 120)); // Dark light gray
 
     // Create 11 hexagons connected to each other in a row
     // Shift hexagons to the right by 1/2 hexagon size further for each colum n
+    int gridOffset = 36; // 36
+    int shiftGrid = gridOffset;
 
     for (int y = 0; y < columns; ++y) {
       for (int x = 0; x < rows; ++x) {
